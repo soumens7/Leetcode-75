a159e98b33a854b9b97d78303962e0088cff3cf4
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-07 00:28:35 +0530

    Time: 63 ms (28.28%) | Memory: 50.5 MB (36.13%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -11,7 +11,8 @@ var kidsWithCandies = function (candies, extraCandies) {
     const result = [];
     // Iterate through the candies array
     for (let i = 0; i < candies.length; i++) {
-        
+        // Check if the current kid, after receiving extra candies,
+        // will have more candies than or equal to the maximum number of candies
         result.push(candies[i] + extraCandies >= maxCandies)
     }
     return result;
