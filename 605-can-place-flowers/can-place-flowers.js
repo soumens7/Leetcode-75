3f5281bcc8f90843c2a357d7d942e3709b6b4139
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-09 00:36:30 +0530

    Time: 53 ms (80.66%) | Memory: 51.2 MB (40.70%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -3,17 +3,18 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
+var canPlaceFlowers = function (flowerbed, n) {
     count = 0;
-    if (n == 0){
+    if (n == 0) {
         return true
     }
-    for(let i =0 ; i< flowerbed.length; i++){
-         if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
             flowerbed[i] = 1; // Plant a flower at the current position
             count++; // Increment the count of planted flowers
         }
     }
-   return n <= count;
-       
+    // Check if the required number of flowers can be planted
+    return n <= count;
+
 };
