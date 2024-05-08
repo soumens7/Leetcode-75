8c1360ec23aaf6cd146be000ecd477e2aee07d3d
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-09 00:32:59 +0530

    Time: 63 ms (33.41%) | Memory: 51 MB (58.77%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    count = 0;
+    if (n == 0){
+        return true
+    }
+    for(let i =0 ; i< flowerbed.length; i++){
+         if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1; // Plant a flower at the current position
+            count++; // Increment the count of planted flowers
+        }
+    }
+    if(n <= count){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
