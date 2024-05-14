3d261420ee2844e14554321d962b330efe43485a
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-14 14:00:05 +0530

    Time: 107 ms (32.34%) | Memory: 61.1 MB (75.20%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    const n = nums.length;
+
+    // Initialize arrays to store prefix and suffix products
+    const prefixProd = new Array(n).fill(1);
+    const suffixProd = new Array(n).fill(1);
+
+    // Calculate prefix products(This array will store the product of all elements to the left of the current element.)
+    for (let i = 1; i < n; i++) {
+        prefixProd[i] = prefixProd[i - 1] * nums[i - 1];
+    }
+    // Calculate suffix products( This array will store the product of all elements to the right of the current element.)
+    for (let i = n - 2; i >= 0; i--) {
+        suffixProd[i] = suffixProd[i + 1] * nums[i + 1];
+    }
+
+    // Calculate answer array
+    const answer = [];
+    for (let i = 0; i < n; i++) {
+        answer[i] = prefixProd[i] * suffixProd[i];
+    }
+
+    return answer;
+};
