354d110f8d13ca5ff62610f80ce589aa2f44bb71
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-20 01:20:26 +0530

    Time: 86 ms (21.48%) | Memory: 54.2 MB (54.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -2,19 +2,17 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-   let count = 0;
-    // first loop will remove all the zeroes
-   for (let i =0; i< nums.length; i++){
-        if(nums[i] == 0){
-            nums.splice(i, 1);
-            count++;
-            i--;
-        }    
-   }  
-   // Second loop will add all the removed zeroes to end of array
-   for (i =0; i< count; i++){
-    nums.push(0);
-   }
-   return nums;
+var moveZeroes = function (nums) {
+    let left = 0; // Initialize left pointer
+
+    // Iterate through the array with the right pointer
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] != 0) {
+            // Swap elements at left and right pointers
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            //Incrementing left pointer
+            left++
+        }
+    }
+    return nums;
 };
