63116d719d443f28593d97cee21a94262b87a520
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-05-21 17:28:11 +0530

    Time: 44 ms (94.66%) | Memory: 48.9 MB (67.76%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,23 +4,20 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-    //! Edge case
-  if (s.length > t.length) return false; //! if len of s is greater than len of t, return false because s cant be a subsequence of t
-  `
-  Example:
-    s='Leetcode'
-    t='Code'
-    here we are trying to find if 'Leetcode' is a subsequence of 'Code' which is not possible because 'Leetcode' is longer than 'Code'
-
-  `;
-  const t_length = t.length;
-  let subsequence = 0;
-  for (let i = 0; i < t_length; i++) {
-    if (s[subsequence] === t[i]) {
-      // ! if it is matching, increment subsequence
-      subsequence++;
+    let i = 0; // pointer for s
+    let j = 0; // pointer for t
+    
+    if (s.length > t.length) {
+        return false;
     }
-  }
-  return subsequence === s.length
-
+    
+    for (j = 0; j < t.length; j++) {
+        if (s[i] === t[j]) {
+            i++;
+        }
+        if (i === s.length) {
+            return true
+        }
+    }
+    return i === s.length;
 };
