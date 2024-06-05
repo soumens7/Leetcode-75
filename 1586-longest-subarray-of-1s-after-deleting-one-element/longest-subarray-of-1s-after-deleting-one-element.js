b9b858ff6af176480ef88173618b779c1350d3e5
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-06-05 10:11:02 +0530

    Time: 75 ms (16.37%) | Memory: 54.6 MB (59.18%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 var longestSubarray = function(nums) {
-    let left = 0;
-    let right = 0;
-    let zeroCount = 0;
-    let maxLen = 0;
-
+    let left = 0; // pointer to slide the window in array
+    let right = 0; // pointer to iterate thorugh the array
+    let zeroCount = 0; // counter to count zeroes 
+    let maxLen = 0; // longest Subarray
+    // Using sliding Window approach
     for(let right = 0 ; right< nums.length; right++){
         if(nums[right] === 0){
             zeroCount++;
         }
+        // if zeroCount exceeds 1 
         while(zeroCount > 1){
             if(nums[left] === 0){
                 zeroCount--;
             }
-
             left++;
         }
+        // getting the Maximum length 
         maxLen = Math.max(maxLen, right-left);
     }
     return maxLen;
