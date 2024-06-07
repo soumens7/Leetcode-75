1c9dc2de0645e772977d1c0753126f2fa958e67e
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-06-07 07:41:06 +0530

    Time: 61 ms (79.60%) | Memory: 51.7 MB (75.83%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -4,24 +4,20 @@
  */
 var pivotIndex = function (nums) {
     let n = nums.length;
-    // Initialize arrays to store prefix and suffix sums
-    const prefixSum = new Array(n).fill(1);
-    const suffixSum = new Array(n).fill(1);
-    // Calculate prefix sums(This array will store the sum of all elements to the left of the current element.)
-    for (let i = 1; i < n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+    // Calculate total sum of array
+    let totalSum = nums.reduce((acc, num) => acc + num, 0);
+//  Initialize leftSum to keep track of the sum of elements to the left of the current index
+let leftSum = 0;
+// Iterate through the array to find the pivot index
+for (let i = 0; i < n; i++) {
+    // Check if the current index is the pivot index
+    // leftSum is the sum of elements to the left of index i
+    // totalSum - leftSum - nums[i] is the sum of elements to the right of index i
+    if (leftSum === totalSum - leftSum - nums[i]) {
+        return i;
     }
-    // Calculate suffix sums(This array will store the sum of all elements to the left of the current element.)
-    for (let i = n - 2; i >= 0; i--) {
-        suffixSum[i] = suffixSum[i + 1] + nums[i + 1];
-    }
-
-    for (let pivot = 0; pivot < n; pivot++) {
-        // condition check pivot Index
-        if (prefixSum[pivot] === suffixSum[pivot]) {
-            return pivot;
-        }
-    }
-    return -1;
-
+    // Update leftSum to include the current element
+    leftSum += nums[i]
+}
+return -1;
 };
