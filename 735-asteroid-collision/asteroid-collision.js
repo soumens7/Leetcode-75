c0594e311e63e1a41b3c69b1f74b1d41cc0bba1d
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-06-20 11:12:57 +0530

    Time: 55 ms (90.64%) | Memory: 51.9 MB (29.10%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -6,28 +6,23 @@ var asteroidCollision = function (asteroids) {
     let stack = [];
 
     for (let asteroid of asteroids) {
-        let shouldPush = true;  // Assume the asteroid should be pushed unless a collision dictates otherwise
+        let shouldPush = true; // Assume asteroid should be pushed 
         while (stack.length > 0 && asteroid < 0 && stack[stack.length - 1] > 0) {
             let top = stack.pop();
             if (Math.abs(asteroid) > top) {
-                // The current negative asteroid is larger; keep popping the stack
-                continue; // Proceed to check the next asteroid in the stack
+                continue;// proceed to check the next asteroid to pop as current one is greater
             } else if (Math.abs(asteroid) === top) {
-                // They are the same size and both explode
-                shouldPush = false; // Do not push the current asteroid
+                shouldPush = false; // do not push the current asteroid as both wil explode
                 break;
             } else {
-                // The top asteroid is larger; push it back and do not push the current one
-                stack.push(top);
+                stack.push(top); // top asteroid is larger so push it back to stack
                 shouldPush = false;
                 break;
             }
         }
-        // If no collision has stopped it and conditions are met, push the current asteroid onto the stack
         if (shouldPush) {
             stack.push(asteroid);
         }
     }
-
     return stack;
 };
