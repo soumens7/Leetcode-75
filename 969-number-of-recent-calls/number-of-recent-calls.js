deaaf678d58b067569b5153a52f5b9170c81b365
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-06-27 09:34:30 +0530

    Time: 166 ms (95.49%) | Memory: 61.2 MB (78.69%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.js
+++ 969-number-of-recent-calls/number-of-recent-calls.js
@@ -11,7 +11,7 @@ RecentCounter.prototype.ping = function (t) {
     this.requests.push(t); // Add the new ping time to the requests array
 
     // Loop to remove all pings from the requests array that are outside the 3000 ms window from the current ping time
-    while (this.requests[0] < t - 3000) {
+    while (this.requests[0] < t - 3000) { // Ex function(3002): (3002 - 3000, 3002)=> 2 - 3002
         this.requests.shift(); // Remove the oldest ping if it's outside the 3000 ms window
     }
     // The length of the queue now represents the count of requests within the last 3000 ms
