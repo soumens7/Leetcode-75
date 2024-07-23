75cecb83c737f39e5f9dfa607b83f3d1c1aef010
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-07-24 00:12:21 +0530

    Time: 51 ms (90.81%) | Memory: 51.7 MB (36.67%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    const leaf_Values = (root, leafVals) => { // function takes a root node and an array leafVals.
+    if(!root){
+        return;
+    }
+    if(!root.left && !root.right){// encounters a leaf node (a node with no left or right child) 
+        leafVals.push(root.val); // it adds the node's value to leafVals.
+    }
+    leaf_Values(root.left, leafVals);
+    leaf_Values(root.right, leafVals);
+   };
+
+   const leafVals1 = []; // for leaf values of root1 
+   const leafVals2 = []; // for leaf values of root2
+   leaf_Values(root1, leafVals1);
+   leaf_Values(root2, leafVals2);
+
+   return JSON.stringify(leafVals1) === JSON.stringify(leafVals2);
+};
