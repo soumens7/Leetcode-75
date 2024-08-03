a8fe1b7399c8104284479aff5e4d2883bcc2d15a
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-08-03 16:34:41 +0530

    Time: 47 ms (100.00%) | Memory: 54.1 MB (66.15%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function(root, targetSum) {
+    let output = 0;
+	let map = {};
+
+	const traverse = (root, pathSum) => {
+		if (!root) return null;
+
+		pathSum += root.val; // current path sum
+
+		// Case 1: starts from root node
+		if (pathSum === targetSum) output++;
+
+		// Case 2: starts from middle of tree
+		// frequency of targetSum in the current path (up to current node)
+		if (map[pathSum - targetSum]) output += map[pathSum - targetSum];
+
+		// memoize current path sum (root to current node) and it's frequency
+		if (map[pathSum]) map[pathSum]++;
+		else map[pathSum] = 1;
+
+		if (root.left) traverse(root.left, pathSum);
+		if (root.right) traverse(root.right, pathSum);
+
+		// remove the current path sum
+		// to note that path is not available/visited
+		map[pathSum]--;
+	};
+
+	traverse(root, 0);
+	return output;
+};
