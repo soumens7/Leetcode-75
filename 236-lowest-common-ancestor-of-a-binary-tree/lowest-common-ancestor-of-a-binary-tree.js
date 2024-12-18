060a8f0646ff9c5408f20b3f8454096e0b530be2
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-12-18 08:50:34 +0530

    Time: 65 ms (71.70%) | Memory: 58.4 MB (95.46%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    // if current node is null or equal to p or q, return it
+    if(!root || root == p || root == q){
+        return root;
+    }
+    // recur for left and right subtree
+    const leftSubtree = lowestCommonAncestor(root.left, p , q);
+    const rightSubtree = lowestCommonAncestor(root.right, p, q);
+
+    // if both left and right are not null, this is the LCA
+    if(leftSubtree && rightSubtree){
+        return root;
+    }
+
+    // Otherwise return left or right
+    return leftSubtree || rightSubtree;
+};
