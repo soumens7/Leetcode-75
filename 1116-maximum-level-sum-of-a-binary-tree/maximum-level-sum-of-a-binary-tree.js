cedb41a1a28d6e1d7592008086a4a3d67f553b32
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2024-12-29 13:00:57 +0530

    Time: 13 ms (72.95%) | Memory: 68.2 MB (72.46%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function (root) {
+    if (!root) return [];
+
+    const queue = [root];// initialize que with root node
+    let maxSum = -Infinity;
+    let maxLevel = 0;
+    let currentLevel = 0;
+
+    while (queue.length > 0) {
+
+        let levelSize = queue.length; // current level size of queue
+        let levelSum = 0; // Sum of node values at current level 
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift(); // Dequeue node
+            levelSum += node.val; // Adding the current node val to levelSum
+
+            // Enqueue left and right children in queue
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        currentLevel++;
+        if (levelSum > maxSum) {
+            maxSum = levelSum;
+            maxLevel = currentLevel;
+        }
+        
+    }
+    return maxLevel;
+};
