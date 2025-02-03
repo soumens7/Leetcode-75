49cb7278d5f40eef9b1172ca08d5a03f237cf343
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-02-03 11:18:54 +0530

    Time: 60 ms (20.23%) | Memory: 49.7 MB (71.35%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// indices = position in array
+var twoSum = function (nums, target) {
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return null;
+};
