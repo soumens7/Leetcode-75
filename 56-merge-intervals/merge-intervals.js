a3f32373fdd35cd8febfe99e717a91e88334bce8
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-02-05 21:15:09 +0530

    Time: 6 ms (90.68%) | Memory: 57.3 MB (97.09%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -7,16 +7,18 @@ var merge = function(intervals) {
 
     intervals.sort((a,b) => a[0] - b[0]); // Sort intervals by start time
 
-    let merged = [intervals[0]]; // Initialize merged list
+    let merged = [intervals[0]]; // Initialize merged array
 
     for (let i =1 ; i< intervals.length; i++){
-        let last = merged[merged.length - 1]; // Initialize last with last element of merged
+
+        let last = merged[merged.length - 1]; // last points to merged[0] 
         let current = intervals[i]; // current interval
 
         if(current[0] <= last[1]){
-            last[1] = Math.max(current[1], last[1]);
+            last[1] = Math.max(current[1], last[1]); // modified merged [0] [1] through reference
         }
         else {
+            // adding no-overlapping interval
             merged.push(current);
         }
     }
