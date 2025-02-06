af01c77e8696fb2b36992ca6ee7058dea96fbab1
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-02-06 15:49:13 +0530

    Time: 157 ms (21.54%) | Memory: 71.8 MB (27.54%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -5,18 +5,18 @@
  */
 var findKthLargest = function (nums, k) {
     // sorting approach
-    nums.sort((a, b) => b - a);
-    return nums[k-1];
+    // nums.sort((a, b) => b - a); // descending order
+    // return nums[k-1];
     // min heap approach
-    // let minHeap = new MinPriorityQueue(); // new min heap
+    let minHeap = new MinPriorityQueue(); // new min heap
 
-    // for (let num of nums) {
-    //     minHeap.enqueue(num); // Add element to heap(minHeap)
-    //     if (minHeap.size() > k) { // if size of queue greater than k
-    //         minHeap.dequeue(); // Remove smallest element in minHeap
-    //     }
-    // }
-    // return minHeap.front().element; // top element is the kth largest
+    for (let num of nums) {
+        minHeap.enqueue(num); // Add element to heap(minHeap)
+        if (minHeap.size() > k) { // if size of queue greater than k
+            minHeap.dequeue(); // Remove smallest element in minHeap
+        }
+    }
+    return minHeap.front().element; // top element is the kth largest
 
 
 };
