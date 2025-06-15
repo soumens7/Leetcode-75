ed7461331fd1fa54667e3c86e8da95ea6177c2f0
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-06-15 13:20:03 +0530

    Time: 45 ms (91.89%) | Memory: 58.5 MB (60.28%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -7,13 +7,18 @@ var removeDuplicates = function (nums) {
     if (nums.length === 0) {
         return 0;
     }
-
+    // Pointer `k` starts from 2 because the first two elements are always allowed
     let k = 2;
     for (let i = 2; i < nums.length; i++) {
+        // Compare the current element with the element k - 2 positions behind
+        // This ensures each element appears at most twice
         if (nums[i] !== nums[k - 2]) {
+            // If it's different, it's allowed -> place it at position k
             nums[k] = nums[i];
-            k++;
+            k++;// Move the write pointer forward
         }
+        // If it's the same as nums[k - 2], skip it (more than 2 duplicates)
     }
+    // Return the new length of the modified array
     return k;
 };
