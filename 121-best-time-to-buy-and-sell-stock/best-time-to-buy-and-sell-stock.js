79922486c2c273b6ef8f783700ac2cd7be5ab746
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-06-22 13:55:15 +0530

    Time: 3 ms (62.65%) | Memory: 65.3 MB (33.77%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if(prices.length < 2){
+        return 0;
+    }
+
+    let minPrice = Infinity;
+
+    let maxProfit = 0;
+
+    for(let i =0; i< prices.length; i++){
+        const currentPrice = prices[i];
+
+        if(currentPrice < minPrice){
+            minPrice = currentPrice;
+        }
+        else{
+            // if currentPrice not lower than minPrice this means we can potentially 
+            // sell at currentPrice for profit
+            const potentialProfit = currentPrice - minPrice; 
+            // check if potentialProfit is greater than maxProfit found so far, 
+            // and update maxProfit accordingly
+            if(potentialProfit > maxProfit){
+                maxProfit = potentialProfit;
+            }
+        }
+    }
+    return maxProfit;
+};
