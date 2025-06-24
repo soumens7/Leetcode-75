49378c63b678ff1d7e123acd59e7158766d966ac
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-06-24 16:32:49 +0530

    Time: 1 ms (74.10%) | Memory: 54.2 MB (52.20%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let max = 0;
+    let start = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        // if day 1 price less than prices[i], it will keep adding up in max 
+        if (start < prices[i]) {
+            max += prices[i] - start;
+        }
+        start = prices[i];
+    }
+
+    return max;
+};
