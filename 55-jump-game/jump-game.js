4ccb97617269e8a0410eac228c6d0ba0b09c0a87
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-06-27 23:44:31 +0530

    Time: 1 ms (83.84%) | Memory: 57.5 MB (86.52%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+
+    let goal = nums.length - 1;// Initialize 'goal' to the last index of the array
+    // Iterate backwards from the second-to-last element down to the first element (index 0)
+    for (let i = nums.length - 1; i >= 0; i--) {
+        // Check if from the current position 'i', we can reach the current 'goal'
+        // 'i + nums[i]' represents the furthest index reachable from 'i'
+        if (i + nums[i] >= goal) {
+            // If we can reach the 'goal' from 'i', it means 'i' now becomes
+            // a new, closer "goal" that we need to reach.
+            // We want to find the leftmost possible index from which we can reach the ultimate goal.
+            goal = i;
+        }
+    }
+    // After checking all positions, if 'goal' has successfully moved back to index 0,
+    // it means we can start from index 0 and reach the last index.
+    return goal === 0;
+};
