d223b1db217f1aee73d0fa47e9d19137cc9df25a
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-07-04 16:19:33 +0530

    Time: 2 ms (46.53%) | Memory: 53.4 MB (86.77%) - LeetSync

--- 274-h-index/h-index.js
+++ 274-h-index/h-index.js
@@ -6,7 +6,7 @@ var hIndex = function (citations) {
     citations.sort((a, b) => a - b); // step 1
     let n = citations.length;
     for (let i = 0; i < n; i++) { // step 2
-        let h = n - i;
+        let h = n - i; // H-index
         if (citations[i] >= h) return h;
     }
     return 0;
