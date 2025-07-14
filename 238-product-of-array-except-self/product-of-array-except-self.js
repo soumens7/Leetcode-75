0940152a1176e39e77d46fee9b98d82fbead721f
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-07-14 13:35:28 +0530

    Time: 3 ms (92.81%) | Memory: 68.4 MB (86.34%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,25 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    const n = nums.length;
+    // Initialize the answer array with all 1s.
+    const answer = Array(nums.length).fill(1);
 
-    // Initialize arrays to store prefix and suffix products
-    const prefixProd = new Array(n).fill(1);
-    const suffixProd = new Array(n).fill(1);
-
-    // Calculate prefix products(This array will store the product of all elements to the left of the current element.)
-    for (let i = 1; i < n; i++) {
-        prefixProd[i] = prefixProd[i - 1] * nums[i - 1];
-    }
-    // Calculate suffix products( This array will store the product of all elements to the right of the current element.)
-    for (let i = n - 2; i >= 0; i--) {
-        suffixProd[i] = suffixProd[i + 1] * nums[i + 1];
+    // answer[i] will contain the product of all elements to the left of i.
+    let left = 1;
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] *= left;
+        left *= nums[i];
     }
 
-    // Calculate answer array(Product of prefix and suffix elements)
-    const answer = [];
-    for (let i = 0; i < n; i++) {
-        answer[i] = prefixProd[i] * suffixProd[i];
+    // answer[i] will contain the product of all elements to the right of i.
+    let right = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= right;
+        right *= nums[i];
     }
 
     return answer;
