3c1337ba107ac218750cdab73b643d7d9a72ba02
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-07-17 00:38:49 +0530

    Time: 4 ms (34.97%) | Memory: 65.9 MB (25.83%) - LeetSync

--- 134-gas-station/gas-station.js
+++ 134-gas-station/gas-station.js
@@ -3,21 +3,20 @@
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
+var canCompleteCircuit = function (gas, cost) {
     if (gas.reduce((a, b) => a + b, 0) < cost.reduce((a, b) => a + b, 0)) {
         return -1;
     }
 
     let currentGas = 0;
-    let start = 0;
+    let startTank = 0;
 
     for (let i = 0; i < gas.length; i++) {
         currentGas += gas[i] - cost[i];
         if (currentGas < 0) {
             currentGas = 0;
-            start = i + 1;
+            startTank = i + 1;
         }
     }
-
-    return start;    
+    return startTank;
 };
