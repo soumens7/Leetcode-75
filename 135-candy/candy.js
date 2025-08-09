14a691658c4a97b70651cd1c414fc3a352528c70
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-08-10 05:18:05 +0530

    Time: 10 ms (28.00%) | Memory: 59.2 MB (27.40%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -3,33 +3,23 @@
  * @return {number}
  */
 var candy = function (ratings) {
-    
-    let n = ratings.length;
-    let totalCandies = n;
-    let i = 1;
-    while (i < n) {
-        if (ratings[i] === ratings[i - 1]) {
-            i++;
-            continue;
-        }
 
-        let currentPeak = 0;
-        while (i < n && ratings[i] > ratings[i - 1]){
-            currentPeak++;
-            totalCandies += currentPeak;
-            i++;
+    let n = ratings.length;
+    let candies = new Array(n).fill(1); // At least 1 candy in each [1, 1, 1]
+
+    // left to right
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
         }
-        if(i === n){
-            return totalCandies;
-        }
-        let currentValley = 0;
-        while(i < n && ratings[i] < ratings[i - 1]){
-            currentValley++;
-            totalCandies += currentValley;
-            i++;
-        }
-        totalCandies -= Math.min(currentPeak, currentValley);
     }
-    return totalCandies;
-    
+    // right to left
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i+1] + 1);
+        }
+    }
+    // sum of candies
+    return candies.reduce((a, b) => a + b, 0);
+
 };
