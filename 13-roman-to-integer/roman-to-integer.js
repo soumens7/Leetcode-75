d59f7a8067bf180523273e6955cfb681076d2cd9
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-08-21 22:50:40 +0530

    Time: 5 ms (53.59%) | Memory: 60.6 MB (73.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var romanToInt = function (s) {
+    // Map of Roman numeral symbols to their integer values
     const romanNum = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     let result = 0;
+    // Loop through each character of the Roman numeral string
     for (let i = 0; i < s.length; i++) {
         if (i+1 < s.length && romanNum[s[i]] < romanNum[s[i + 1]]) {
             result -= romanNum[s[i]];
