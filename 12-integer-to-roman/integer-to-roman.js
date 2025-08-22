9efe406a60051305c6ab3d0d8f6b4ada459f029f
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-08-22 20:08:38 +0530

    Time: 7 ms (47.60%) | Memory: 64.4 MB (10.57%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.js
+++ 12-integer-to-roman/integer-to-roman.js
@@ -3,16 +3,23 @@
  * @return {string}
  */
 var intToRoman = function (num) {
-    // Map of Roman numeral symbols to their integer values
+    // Mapping of Roman numeral symbols to their integer values
+    // Ordered from largest to smallest (important for greedy approach)
     const valSymbols = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
     ];
 
-    let romanNum = ''; // list
+    let romanNum = ''; // Final Roman numeral string to be build
 
     for (const [val, symbol] of valSymbols) {
         if (num === 0) break;
-        let count = Math.floor(num / val); // 3759 / 1000 = 3
-        romanNum += symbol.repeat(count); //3 * M
+       // How many times the current Roman symbol can fit into num
+        let count = Math.floor(num / val);
+
+        // Append that many symbols to the result string
+        // Example: if num = 3000, val = 1000 → 'M'.repeat(3) → 'MMM'
+        romanNum += symbol.repeat(count); 
+
+        // Reduce the number by the value just converted
         num -= count * val;
     }
     return romanNum;
