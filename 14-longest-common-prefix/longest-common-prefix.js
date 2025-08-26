30f3211d9e2d184f6f2d113826f0f3fdb40f6243
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-08-26 17:09:25 +0530

    Time: 7 ms (10.16%) | Memory: 57.2 MB (7.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -5,9 +5,9 @@
 function longestCommonPrefix(strs) {
     if(!strs.length) return "";
 
-    for (let i = 0; i < strs[0].length; i++) {
+    for (let i = 0; i < strs[0].length; i++) {//Scan each char position i of the first string
         let char = strs[0][i];
-        for (let j = 1; j < strs.length; j++) { // to compare all strings to first string
+        for (let j = 1; j < strs.length; j++) { //Compare this char with all other strings at pos i
             if (strs[j][i] !== char) {
                 return strs[0].slice(0, i);
             }
