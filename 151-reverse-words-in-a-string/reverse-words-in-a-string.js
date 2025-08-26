9f65cf2386e756b50c9cfd5496963e2c555d924e
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-08-26 18:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (87.09%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,10 +3,13 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    if(!s.length) return "";
+    let words = s.split(' ');//line splits the input string s into an array of substrings (words) using a space as the delimiter
+    let res = [];
 
-    let words = s.trim().split(/\s+/); // trim string and split into words 
-    words.reverse(); // reverse the words
-
-    return words.join(' ');// join the words with single space
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) {
+            res.push(words[i]);//if true added to the end of the res array.
+        }
+    }
+    return res.join(' ');//res array is joined back into a single string with a space ' '
 };
