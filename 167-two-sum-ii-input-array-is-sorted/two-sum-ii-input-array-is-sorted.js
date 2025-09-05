3e6663940d93ec30b7230aa42d207153fdd7624a
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-09-05 17:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (65.08%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+
+    let left = 0, right = numbers.length - 1;
+
+    while(left < right){
+        let sum = numbers[left] + numbers[right];
+        if(sum < target){
+            left++;
+        }
+        else if(sum > target){
+            right--;
+        }
+        else{
+            return [left + 1, right +1]
+        }
+        
+    }
+};
