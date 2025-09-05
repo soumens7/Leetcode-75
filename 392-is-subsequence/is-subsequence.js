1cc5b4b18d85fceee75eaae05608d49478e0aa40
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-09-05 13:47:22 +0530

    Time: 1 ms (72.81%) | Memory: 54.2 MB (87.31%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -3,24 +3,16 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function (s, t) {
-    let i = 0; // pointer for s
-    let j = 0; // pointer for t
-    
-    // edge case 
-    if (s.length > t.length) {
-        return false;
-    }
-    
-    for (j = 0; j < t.length; j++) {
-        // checks for identical elements in string t
-        if (s[i] === t[j]) {
+var isSubsequence = function(s, t) {
+    if(s.length === 0) return true;
+    let i = 0, j = 0;
+
+    while(i < s.length && j < t.length){
+        if(s[i] === t[j]){
             i++;
         }
-        // checks if i reached to the end of string s
-        if (i === s.length) {
-            return true
-        }
-    }
-    return i === s.length;
+        j++;
+        if(i === s.length) return true;
+    } 
+    return false;
 };
