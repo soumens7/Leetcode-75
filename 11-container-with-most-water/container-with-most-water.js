4f1a71f4064087d3d1b896015047ae7de253f29a
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-09-09 01:06:19 +0530

    Time: 2 ms (81.40%) | Memory: 63.5 MB (24.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -4,15 +4,15 @@
  */
 var maxArea = function(height) {
     let left = 0, right = height.length -1;
-    let maxAmnt = 0;
+    let maxArea = 0;
     while(left < right){
         const area = Math.min(height[left], height[right]) * (right - left);
-        maxAmnt = Math.max(maxAmnt, area);
+        maxArea = Math.max(maxArea, area);
         if(height[left] < height[right]){
             left++;
         } else{
             right--;
         }
     }
-    return maxAmnt;
+    return maxArea;
 };
