5111b57e3837d676fef25e67f0cf25771d37cfed
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-09-09 01:01:49 +0530

    Time: 1 ms (95.18%) | Memory: 63.7 MB (12.64%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,23 +2,17 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
-    let left = 0; // Starting Pointer
-    let right = height.length - 1;// Ending pointer
-    let max_area = 0; // Initialize Maximum Area
-
-    while (left < right) {
-        // Calculate the area
-        let width = right - left;
-        let current_area = Math.min(height[left], height[right]) * width;
-        // Update the maximum area if current area is greater
-        max_area = Math.max(max_area, current_area);
-        // Moving the pointer 
-        if (height[left] < height[right]) {
+var maxArea = function(height) {
+    let left = 0, right = height.length -1;
+    let maxAmnt = 0;
+    while(left < right){
+        const area = Math.min(height[left], height[right]) * (right - left);
+        maxAmnt = Math.max(maxAmnt, area);
+        if(height[left] < height[right]){
             left++;
-        } else {
+        } else{
             right--;
         }
     }
-    return max_area;
+    return maxAmnt;
 };
