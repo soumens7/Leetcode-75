b4bb89b758810be58394659aa8a314ff6249c190
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-09-29 22:23:33 +0530

    Time: 9 ms (41.77%) | Memory: 56.8 MB (83.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let map = new Map(); // hash map to store characters of s
-    let left = 0, maxLen = 0;
-    
-    for(let right = 0; right < s.length; right++){
-        if(map.has(s[right]) && (map.get(s[right]) >= left)){ // check for repeating character in map
-            left = map.get(s[right]) + 1;
+    let left = 0, maxLen= 0 ;
+    let charSet = new Set();
+    for(let right =0; right<s.length; right++){
+        while(charSet.has(s[right])){
+            charSet.delete(s[left]);
+            left++;
         }
-        map.set(s[right], right); //storing string elements in map
-        maxLen = Math.max(maxLen, right - left + 1); // check for max unrepeated length in s
-    }
-    return maxLen; // return max length 
+        charSet.add(s[right]);
+        maxLen = Math.max(maxLen, right-left + 1)
+    }   
+    return maxLen;
 };
