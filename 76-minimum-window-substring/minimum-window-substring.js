6951195615b2b9cb4174c9f2b1e9d4ae23673deb
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-11-02 23:14:22 +0530

    Time: 41 ms (19.47%) | Memory: 56.5 MB (86.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -17,10 +17,9 @@ var minWindow = function (s, t) {
     let left = 0, formed = 0;
     const have = new Map();
     const required = charCount.size;
+    let minLen = Infinity, minStart = 0;
 
-    // best window
-    let ansLen = Infinity, ansL = 0, ansR = 0;
-
+    // Expand Window
     for (let right = 0; right < s.length; right++) {
         const c = s[right];
         have.set(c, (have.get(c) || 0) + 1);
@@ -30,16 +29,18 @@ var minWindow = function (s, t) {
         // when window covers t, shrink it from left
         while (formed === required) {
             // update best answer
-            if (right - left + 1 < ansLen) {
-                ansLen = right - left + 1;
-                ansL = left;
-                ansR = right;
+            if (right - left + 1 < minLen) {
+                minLen = right - left + 1;
+                minStart = left;
+
             }
             const d = s[left];
             have.set(d, have.get(d) - 1);
             if (charCount.has(d) && have.get(d) < charCount.get(d)) formed--;
             left++;
         }
+
     }
-    return ansLen === Infinity ? "" : s.slice(ansL, ansR + 1);
+    // return result
+    return minLen === Infinity ? "" : s.slice(minStart, minStart + minLen);
 };
