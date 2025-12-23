8aaae9adc400a5b783b40d2dd5a8b67dc390cb0b
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2025-12-24 00:26:22 +0530

    Time: 1 ms (13.47%) | Memory: 54.2 MB (87.68%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length;
+
+    // Transpose in-place
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+};
