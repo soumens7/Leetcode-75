bf908226aef4a8677a9dc4c9d7c8085afb416955
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2026-05-30 02:36:41 +0530

    Time: 33 ms (53.94%) | Memory: 67.2 MB (67.55%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for(let str of strs){
+        const key = str.split('').sort().join('');
+        if(!map.has(key)){
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+    return Array.from(map.values());
+};
