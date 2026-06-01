02b4a7e01ccbdd45852a952d7b099d305e85b93c
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2026-06-01 06:19:00 +0530

    Time: 32 ms (57.71%) | Memory: 67.1 MB (70.34%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,15 +2,18 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
     const map = new Map();
 
-    for(let str of strs){
-        const key = str.split('').sort().join('');
-        if(!map.has(key)){
+    for (let str of strs) {
+        let key = str.split('').sort().join();
+        if (!map.has(key)) {
+            // create key with empty map
             map.set(key, []);
         }
+        // skip creation if key exist, push value(str) to key(array)
         map.get(key).push(str);
     }
+    // extracts right hand side of map
     return Array.from(map.values());
 };
