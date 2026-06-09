7ca295eeb78e03e382db994641c4b543d20cec76
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2026-06-09 17:03:45 +0530

    Time: 9 ms (51.13%) | Memory: 64.5 MB (69.99%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -2,25 +2,26 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-    if (intervals.length === 0) return [];
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [];
+    // intialize prev with first interval from sorted list
+    let prev = intervals[0]; 
 
-    intervals.sort((a,b) => a[0] - b[0]); // Sort intervals by start time
-
-    let merged = [intervals[0]]; // Initialize merged array
-
-    for (let i =1 ; i< intervals.length; i++){
-
-        let last = merged[merged.length - 1]; // last points to merged[0] 
-        let current = intervals[i]; // current interval
-
-        if(current[0] <= last[1]){
-            last[1] = Math.max(current[1], last[1]); // modified merged [0] [1] through reference
-        }
-        else {
-            // adding non-overlapping interval
-            merged.push(current);
+    // Iterating through intervals
+    for (let i = 1; i < intervals.length; i++) {
+        let interval = intervals[i]; 
+        // checking overlap and Merging
+        if (interval[0] <= prev[1]) { 
+            prev[1] = Math.max(prev[1], interval[1]); 
+        } else {
+            merged.push(prev); 
+            // update prev with current interval
+            prev = interval; 
         }
     }
-    return merged;
+    // adding last interval
+    merged.push(prev); 
+    return merged; 
+
 };
