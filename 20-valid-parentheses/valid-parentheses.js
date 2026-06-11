577471a4e8f5e2ac2659631693a34d35ac68369e
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2026-06-11 13:52:27 +0530

    Time: 9 ms (8.52%) | Memory: 56.7 MB (29.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,20 +3,21 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    let stack = []; // empty stack to store opening bracket
-    for (char of s) {
-        if (char === '(' || char === '{' || char === '[') {
-            stack.push(char); // push it onto stack
-        } else { // if char is closing bracket
-            if (!stack.length || // if stack is empty 
-                (char === ')' && stack[stack.length - 1] !== '(') || // closing bracket should match corresponding opening bracket
-                (char === '}' && stack[stack.length - 1] !== '{') ||
-                (char === ']' && stack[stack.length - 1] !== '[')) {
-                return false; // string is not valid
+    const stack = [];
+     const mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+    for (let c of s) {
+        if(Object.values(mapping).includes(c)){
+            stack.push(c);
+        }
+        else if(mapping.hasOwnProperty(c)){
+            if(!stack.length || mapping[c] !== stack.pop()){
+                return false;
             }
-            stack.pop(); // if invalid found, pop the opening bracket from stack
         }
     }
-    return !stack.length; // if stack is empty, all opening brackets are matched with closing brackets, will return true
-                         // string will be invalid if unmatches brackets found, will return false 
+    return !stack.length;
 };
