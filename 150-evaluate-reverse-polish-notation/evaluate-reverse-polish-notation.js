385f901815e04209a6d6f084c61b4212f89339cb
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2026-06-16 14:33:28 +0530

    Time: 5 ms (71.76%) | Memory: 56.9 MB (75.82%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    const stack = [];
+
+    for (let c of tokens) {
+        if (c === "+") {
+            stack.push(stack.pop() + stack.pop());
+        } else if (c === "-") {
+            let second = stack.pop();
+            let first = stack.pop();
+            stack.push(first - second);
+        } else if (c === "*") {
+            stack.push(stack.pop() * stack.pop());
+        } else if (c === "/") {
+            let second = stack.pop();
+            let first = stack.pop();
+            stack.push(parseInt(first / second));
+        } else {
+            stack.push(parseInt(c));
+        }
+
+    }
+    return stack[0];
+
+};
